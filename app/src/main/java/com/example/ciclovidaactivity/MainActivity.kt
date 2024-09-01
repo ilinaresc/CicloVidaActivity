941d6ce2dec9daea71ae5b8c9062baf35c3619fc
Irving Linares <linares.irving@gmail.com>
2024-09-01 13:02:18 -0500

    Se imprimen los estados de la actividad

--- app/src/main/java/com/example/ciclovidaactivity/MainActivity.kt
+++ app/src/main/java/com/example/ciclovidaactivity/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.ciclovidaactivity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private val tag: String = "INF-STATE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.i(tag,"MainActivity-->onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag,"MainActivity-->onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag,"MainActivity-->onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag,"MainActivity-->onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag,"MainActivity-->onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag,"MainActivity-->onDestroy")
     }
 }
